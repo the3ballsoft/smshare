8e6053ed86b366ec7a9f424c229e6241fc969094
Genesis Guerrero <genesisdaft@gmail.com>
2014-10-08 03:52:20 -0500

    menu principal con filtro

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 var eleOpcionesLugares = document.getElementById('opcionesLugares');
+var btnActividades = document.getElementById("opt_actividades");
+var btnTipoLugar = document.getElementById("opt_tipolugar");
 
-var app = {
-	init: function(){
-		 cargarOptActividades();
-	},
-};
 
 function cargarOptActividades(){
 	 var xhReq = new XMLHttpRequest();
@@ -15,8 +12,23 @@ function cargarOptActividades(){
 }
 function cargarOptTipolugar(){
 	 var xhReq = new XMLHttpRequest();
-	 xhReq.open("GET", "html/tipolugares.html", false);
+	 xhReq.open("GET", "html/tipolugares_menu.html", false);
 	 xhReq.send(null);
 	 var respuesta = xhReq.responseText;
 	 eleOpcionesLugares.innerHTML = respuesta;
 }
+
+var app = {
+	init: function(){
+		 cargarOptActividades();
+		 $('.btn').click(function() {
+		 	if( ($(this))[0].childNodes[1] == btnActividades ){
+		 		cargarOptActividades();
+		 	}
+		 	if( ($(this))[0].childNodes[1] == btnTipoLugar ){
+		 		cargarOptTipolugar();
+		 	}
+	   	 });
+	},
+};
+
